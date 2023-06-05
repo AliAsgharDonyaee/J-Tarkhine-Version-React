89e0f7a42e4db737085c3bd17f88027278908352
AliasgharDevF <aliasghardef@gmail.com>
2023-06-05 12:48:02 +0100

    add page cart

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,9 +1,17 @@
-import React from "react";
+import Image from "../assets/images/404_image/Vector.svg";
+import WideComponents from "../components/WideComponents";
 
 const CartPage = () => {
 	return (
 		<section className='w-full h-auto'>
-			<div className='mx-auto w-[95%] xl:w-[1024px] 2xl:w-[1440px]'> cart</div>
+			<div className='w-full min-h-screen'>
+				<WideComponents>
+					<div className='mt-7x md:mt-9x lg:mt-10x xl:mt-11x 2xl:mt-12x w-full h-36 center-items-flex flex-col'>
+						<h3 className='text-Neutral-Gray-8 xl:text-3x'>شما در حال حاظر هیچ سفارشی ندارید</h3>
+						<img src={Image} alt='not found img' className='mt-3x w-1/2 md:w-1/3 xl:w-1/5' />
+					</div>
+				</WideComponents>
+			</div>
 		</section>
 	);
 };
